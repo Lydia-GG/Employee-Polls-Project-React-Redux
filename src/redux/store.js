7eba0cd290d7b773aec6efef0695f8325f295a50
Lydia Samir <L-Samir@hotmail.com>
2022-08-09 22:20:33 +0200

    a

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,7 @@
 import { createStore } from 'redux';
 import reducer from './reducers';
 import middleware from './middleware';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(reducer, composeWithDevTools(middleware));
+const store = createStore(reducer, middleware);
 
 export default store;
